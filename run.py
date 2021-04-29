7da50b0362bdb70258d00bfe88590fd10e00614c
Raymond <r.danjou@student.tudelft.nl>
2021-04-29 14:28:30 +0200

    f

--- run.py
+++ run.py
@@ -1,53 +1,189 @@
-import yaml
-import argparse
-import numpy as np
+import math
+import torch
+from torch import optim
+from models import BaseVAE
+from models.types_ import *
+from utils import data_loader
+import pytorch_lightning as pl
+from torchvision import transforms
+import torchvision.utils as vutils
+from torchvision.datasets import CelebA
+from torch.utils.data import DataLoader
+import dill
+import pickle
+class VAEXperiment(pl.LightningModule):
 
-from models import *
-from experiment import VAEXperiment
-import torch.backends.cudnn as cudnn
-from pytorch_lightning import Trainer
-from pytorch_lightning.loggers import TestTubeLogger
+    def __init__(self,
+                 vae_model: BaseVAE,
+                 params: dict) -> None:
+        super(VAEXperiment, self).__init__()
+
+        self.model = vae_model
+        self.params = params
+        self.curr_device = None
+        self.hold_graph = False
+        try:
+            self.hold_graph = self.params['retain_first_backpass']
+        except:
+            pass
+
+    def forward(self, input: Tensor, **kwargs) -> Tensor:
+        return self.model(input, **kwargs)
+
+    def training_step(self, batch, batch_idx, optimizer_idx = 0):
+        real_img, labels = batch
+        self.curr_device = real_img.device
+
+        results = self.forward(real_img, labels = labels)
+        train_loss = self.model.loss_function(*results,
+                                              M_N = self.params['batch_size']/ self.num_train_imgs,
+                                              optimizer_idx=optimizer_idx,
+                                              batch_idx = batch_idx)
+
+        self.logger.experiment.log({key: val.item() for key, val in train_loss.items()})
+
+        return train_loss
+
+    def validation_step(self, batch, batch_idx, optimizer_idx = 0):
+        real_img, labels = batch
+        self.curr_device = real_img.device
+
+        results = self.forward(real_img, labels = labels)
+        val_loss = self.model.loss_function(*results,
+                                            M_N = self.params['batch_size']/ self.num_val_imgs,
+                                            optimizer_idx = optimizer_idx,
+                                            batch_idx = batch_idx)
+
+        return val_loss
+
+    def validation_end(self, outputs):
+        avg_loss = torch.stack([x['loss'] for x in outputs]).mean()
+        tensorboard_logs = {'avg_val_loss': avg_loss}
+        self.sample_images()
+        return {'val_loss': avg_loss, 'log': tensorboard_logs}
+
+    def sample_images(self):
+        # Get sample reconstruction image
+        test_input, test_label = next(iter(self.sample_dataloader))
+        test_input = test_input.to(self.curr_device)
+        test_label = test_label.to(self.curr_device)
+        recons = self.model.generate(test_input, labels = test_label)
+        vutils.save_image(recons.data,
+                          f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
+                          f"recons_{self.logger.name}_{self.current_epoch}.png",
+                          normalize=True,
+                          nrow=12)
+
+        # vutils.save_image(test_input.data,
+        #                   f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
+        #                   f"real_img_{self.logger.name}_{self.current_epoch}.png",
+        #                   normalize=True,
+        #                   nrow=12)
+
+        try:
+            samples = self.model.sample(144,
+                                        self.curr_device,
+                                        labels = test_label)
+            vutils.save_image(samples.cpu().data,
+                              f"{self.logger.save_dir}{self.logger.name}/version_{self.logger.version}/"
+                              f"{self.logger.name}_{self.current_epoch}.png",
+                              normalize=True,
+                              nrow=12)
+        except:
+            pass
 
 
-parser = argparse.ArgumentParser(description='Generic runner for VAE models')
-parser.add_argument('--config',  '-c',
-                    dest="filename",
-                    metavar='FILE',
-                    help =  'path to the config file',
-                    default='configs/vae.yaml')
-
-args = parser.parse_args()
-with open(args.filename, 'r') as file:
-    try:
-        config = yaml.safe_load(file)
-    except yaml.YAMLError as exc:
-        print(exc)
+        del test_input, recons #, samples
 
 
-tt_logger = TestTubeLogger(
-    save_dir=config['logging_params']['save_dir'],
-    name=config['logging_params']['name'],
-    debug=False,
-    create_git_tag=False,
-)
+    def configure_optimizers(self):
 
-# For reproducibility
-torch.manual_seed(config['logging_params']['manual_seed'])
-np.random.seed(config['logging_params']['manual_seed'])
-cudnn.deterministic = True
-cudnn.benchmark = False
+        optims = []
+        scheds = []
 
-model = vae_models[config['model_params']['name']](**config['model_params'])
-experiment = VAEXperiment(model,
-                          config['exp_params'])
+        optimizer = optim.Adam(self.model.parameters(),
+                               lr=self.params['LR'],
+                               weight_decay=self.params['weight_decay'])
+        optims.append(optimizer)
+        # Check if more than 1 optimizer is required (Used for adversarial training)
+        try:
+            if self.params['LR_2'] is not None:
+                optimizer2 = optim.Adam(getattr(self.model,self.params['submodel']).parameters(),
+                                        lr=self.params['LR_2'])
+                optims.append(optimizer2)
+        except:
+            pass
 
-runner = Trainer(default_root_dir=f"{tt_logger.save_dir}",
-                 min_epochs=1,
-                 logger=tt_logger,
-                 flush_logs_every_n_steps=1000,
-                 num_sanity_val_steps=5,
-                 benchmark=True,
-                 **config['trainer_params'])
+        try:
+            if self.params['scheduler_gamma'] is not None:
+                scheduler = optim.lr_scheduler.ExponentialLR(optims[0],
+                                                             gamma = self.params['scheduler_gamma'])
+                scheds.append(scheduler)
 
-print(f"======= Training {config['model_params']['name']} =======")
-runner.fit(experiment.cuda(0))
+                # Check if another scheduler is required for the second optimizer
+                try:
+                    if self.params['scheduler_gamma_2'] is not None:
+                        scheduler2 = optim.lr_scheduler.ExponentialLR(optims[1],
+                                                                      gamma = self.params['scheduler_gamma_2'])
+                        scheds.append(scheduler2)
+                except:
+                    pass
+                return optims, scheds
+        except:
+            return optims
+
+    @data_loader
+    def train_dataloader(self):
+        transform = self.data_transforms()
+
+        if self.params['dataset'] == 'celeba':
+            dataset = CelebA(root = self.params['data_path'],
+                             split = "train",
+                             transform=transform,
+                             download=False)
+        else:
+            raise ValueError('Undefined dataset type')
+
+        self.num_train_imgs = len(dataset)
+        return DataLoader(dataset,
+                          batch_size= self.params['batch_size'],
+                          shuffle = False,
+                          drop_last=True, prefetch_factor=4, num_workers=3, pin_memory=False)
+
+    @data_loader
+    def val_dataloader(self):
+        transform = self.data_transforms()
+
+        if self.params['dataset'] == 'celeba':
+            self.sample_dataloader =  DataLoader(CelebA(root = self.params['data_path'],
+                                                        split = "test",
+                                                        transform=transform,
+                                                        download=False),
+                                                 batch_size= 144,
+                                                 shuffle = False,
+                                                 drop_last=True, prefetch_factor=8, num_workers=3, pin_memory=False)
+
+            self.num_val_imgs = len(self.sample_dataloader)
+        else:
+            raise ValueError('Undefined dataset type')
+
+        return self.sample_dataloader
+
+    def data_transforms(self):
+
+
+        SetRange = transforms.Lambda(dummy_thicc)
+        SetScale = transforms.Lambda(lambda X: X/X.sum(0).expand_as(X))
+
+        if self.params['dataset'] == 'celeba':
+            transform = transforms.Compose([transforms.RandomHorizontalFlip(),
+                                            transforms.CenterCrop(148),
+                                            transforms.Resize(self.params['img_size']),
+                                            transforms.ToTensor(),
+                                            SetRange])
+        else:
+            raise ValueError('Undefined dataset type')
+        return transform
+
+def dummy_thicc(x):
+    return 2 * x - 1.
