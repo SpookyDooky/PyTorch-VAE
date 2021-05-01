2fcd0edd8c472ada62f1215e046b1c716bfb2c98
Raymond <r.danjou@student.tudelft.nl>
2021-05-01 18:10:13 +0200

    Implemented the data loaders for validation and training of the custom dataset

--- test.py
+++ test.py
@@ -1,3 +1,3 @@
-from celdata import CellDataset
+from celldata import CellDataset
 
 cellStuff = CellDataset("C:\\Users\\Raymond\\Documents\\Programming\\Research project\\PyTorch-VAE\\dataset\\celldata\\", 300)
